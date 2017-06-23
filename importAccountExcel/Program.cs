a0ca4ce063dd2c71e734f1485ae170bbd5a89b66
prabaei <prabaei@gmail.com>
2017-06-23 12:52:29 +0530

    new addition

--- importAccountExcel/Program.cs
+++ importAccountExcel/Program.cs
@@ -1,6 +1,8 @@
 ﻿using importAccountExcel.Data;
+using importAccountExcel.TallyDb;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,30 +24,30 @@ namespace importAccountExcel
             {
                 for (int j = 1; j <= xlrange.Columns.Count; j++)
                 {
-                    
-                        //Console.Write(string.Format("cells[{0},{1}] ", i, j));
-                    if(j==1)
+
+                    //Console.Write(string.Format("cells[{0},{1}] ", i, j));
+                    if (j == 1)
                     {
-                        if(xlrange.Cells[i, j].Value2!=null)
+                        if (xlrange.Cells[i, j].Value2 != null)
                         {
                             if (!string.IsNullOrEmpty(xlrange.Cells[i, j].Value2.ToString()))
                             {
                                 double dt = double.Parse(xlrange.Cells[i, j].Value2.ToString());
                                 DateTime time = DateTime.FromOADate(dt);
-                               // Console.WriteLine("found new master record...");
-                                string balance=xlrange.Cells[i, 13].Value2.ToString();
-                               // balance=balance.Substring(balance.Length - 3);
+                                // Console.WriteLine("found new master record...");
+                                string balance = xlrange.Cells[i, 13].Value2.ToString();
+                                // balance=balance.Substring(balance.Length - 3);
                                 MasterTable mt = new MasterTable()
                                 {
                                     dt = Convert.ToDateTime(time),
                                     toby = xlrange.Cells[i, 2].Value2.ToString(),
-                                    particular=xlrange.Cells[i,3].Value2.ToString(),
-                                    voucherType= xlrange.Cells[i, 9].Value2.ToString(),
-                                    vchno= Convert.ToInt16(xlrange.Cells[i, 10].Value2.ToString()),
-                                    debit= Convert.ToDecimal(xlrange.Cells[i, 11].Value2),
-                                    credit=Convert.ToDecimal(xlrange.Cells[i, 12].Value2),
-                                    balance=Convert.ToDecimal(balance),
-                                    AccountDetail= xlrange.Cells[4, 1].Value2.ToString()
+                                    particular = xlrange.Cells[i, 3].Value2.ToString(),
+                                    voucherType = xlrange.Cells[i, 9].Value2.ToString(),
+                                    vchno = Convert.ToInt16(xlrange.Cells[i, 10].Value2.ToString()),
+                                    debit = Convert.ToDecimal(xlrange.Cells[i, 11].Value2),
+                                    credit = Convert.ToDecimal(xlrange.Cells[i, 12].Value2),
+                                    balance = Convert.ToDecimal(balance),
+                                    AccountDetail = xlrange.Cells[4, 1].Value2.ToString()
                                 };
                                 database.MasterTable.Add(mt);
                                 database.SaveChanges();
@@ -57,14 +59,14 @@ namespace importAccountExcel
                                 decimal? amt = null;
                                 double? dateval = xlrange.Cells[i, 5].Value2;
                                 var stramt = xlrange.Cells[i, 6].Value2;
-                                if (stramt!=null)
+                                if (stramt != null)
                                 {
                                     amt = Convert.ToDecimal(stramt);
                                 }
                                 if (dateval != null)
                                 {
                                     double dt2 = double.Parse(xlrange.Cells[i, 5].Value2.ToString());
-                                     time2 = DateTime.FromOADate(dt2);
+                                    time2 = DateTime.FromOADate(dt2);
                                 }
 
                                 secMstr secmaster = new secMstr()
@@ -78,14 +80,14 @@ namespace importAccountExcel
                                 database.secMstr.Add(secmaster);
                                 database.SaveChanges();
 
-                        }
+                            }
 
                         }
                     }
-                    
+
                     //if (xlrange.Cells[i, j].Value2 != null)
                     //{
-                        
+
                     //   // Console.Write(xlrange.Cells[i, j] == null ? " " : xlrange.Cells[i, j].Value2.ToString());
 
                     //}
@@ -93,6 +95,21 @@ namespace importAccountExcel
                 }
                 int percentage = (i * 100) / 3351;
                 Console.Write("\r{0}%   ", percentage);
+            }
+        }
+    }
+
+
+    public class normalising{
+        public static ICSRDBTALLYEntities icsrdbtally = new ICSRDBTALLYEntities();
+        SqlConnection con = new SqlConnection("");
+        public void chenageIt()
+        {
+            SqlCommand sqlcmd = new SqlCommand(@"select * from [ICSRDBTALLY].[dbo].[Vouchers] where LedgerName not like 'NALLAYARASU A/c 2722101012096' and ISNULL(void,'N')='N' and TallyMasterid in(select TallyMasterid from [ICSRDBTALLY].[dbo].[Vouchers] where LedgerName like 'NALLAYARASU A/c 2722101012096' and ISNULL(void,'N')='N') order by VoucherDate asc", new SqlConnection(@"Data Source = USER1 - PC; Initial Catalog = FACCT; Integrated Security = False; User Id = sa; Password = IcsR@123#;MultipleActiveResultSets=True"));
+          SqlDataReader dr=  sqlcmd.ExecuteReader();
+            while (dr.Read())
+            {
+                
             }
         }
     }
